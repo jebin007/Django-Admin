aa33f1cabe1c11375b5f0cfd1747e2d279303664
jebin007 <jevinsubedi@gmail.com>
2017-07-15 03:49:54 -0500

    Building Custom Filters:
    -added a new filter in courses to filter courses by the year created on 2015 and 2016
    -title = is the title that appears after the word By" at the admin.
    -parameter_name is the name that gets used in the url whenver we use the filter.
    -lookups function will create the clickable links in the right hand side of the sidebar.
    -one of the value inside a tuple will go in the sidebar and other will go in the url. ('2015', '2015')
    -finally we need to add a method called queryset that will return all of the objects that fit the parameter of our queryset.

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from datetime import date
 
 from . import models
 
@@ -14,11 +15,30 @@ class QuizInline(admin.StackedInline):
 class AnswerInline(admin.StackedInline):
     model = models.Answer
 
+class YearListFilter(admin.SimpleListFilter):
+    title = 'year created'
+    parameter_name = 'year'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('2015', '2015'),
+            ('2016', '2016'),
+
+        )
+
+    def queryset(self, request, queryset):
+        if self. value() == '2015':
+            return queryset.filter(created_at__gte=date(2015, 1, 1),
+                                   created_at__lte=date(2015, 12, 31))
+        if self. value() == '2016':
+            return queryset.filter(created_at__gte=date(2016, 1, 1),
+                                   created_at__lte=date(2016, 12, 31))
+
     
 class CourseAdmin(admin.ModelAdmin):
     inlines = [TextInline, QuizInline]
     search_fields = ['title', 'description']
-    list_filter = ['created_at', 'is_live']
+    list_filter = ['created_at', 'is_live', YearListFilter]
     
     
 class QuestionAdmin(admin.ModelAdmin):
